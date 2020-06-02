83969aef9f182acdcdbce5226f524a2a2ac68dd2
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-06-02 11:16:41 +0500

    add video

---
+++ js/totop.js
@@ -0,0 +1,33 @@
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+function loadData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, 500);
+    })
+}
+
+loadData()
+    .then(() => {
+        let preloaderEl = document.getElementById('preloader');
+        preloaderEl.classList.add('hidden');
+        preloaderEl.classList.remove('visible');
+    });
